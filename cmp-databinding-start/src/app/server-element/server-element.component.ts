f8b35acec89c8c398009ebdf9de2a33bd51491b0
Pa1K <pavan.ntr9@gmail.com>
2018-02-17 22:09:50 +0530

    Implement Angular LifeCycle Hooks

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit,Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit,Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +7,54 @@ import { Component, OnInit,Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation:ViewEncapsulation.Emulated // None,Native also available
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+OnInit, 
+OnChanges,
+DoCheck, 
+AfterContentInit, 
+AfterContentChecked, 
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
+ 
   @Input('srvElement') element : {type : string, name: string, content: string};
+  @Input() name: string;
   constructor() {
     console.log('constructor called');
    }
 
+   ngOnChanges(changes: SimpleChanges) {
+     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+     //Add '${implements OnChanges}' to the class.
+     console.log('ngOnChanges called');
+     console.log(changes);
+     
+   }
   ngOnInit() {
     console.log('ngOnInit called');
   }
 
+  ngDoCheck(){
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+
+  }
+  ngOnDestroy(){
+    console.log('ngOnDestroy called');
+}
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called');
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called');
+
+  }
+ 
 }
